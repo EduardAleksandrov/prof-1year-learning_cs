acd6419d6605bac2c98857bf0b10515e247b6983
Eduard Aleksandrov <eduard.ale@list.ru>
2025-12-18 16:41:41 +0300

    ling test

--- 2.linq_v1/Program.cs
+++ 2.linq_v1/Program.cs
@@ -4,7 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string s1 = "bbab";
+        string s2 = "bbbb";
+
+        bool checkTrue= s1.Any(s => s != s1[0]);
+        Console.WriteLine(checkTrue);
+
+        bool checkFalse= s2.Any(s => s != s1[0]);
+        Console.WriteLine(checkFalse);
     }
-    
 }
